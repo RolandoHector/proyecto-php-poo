cc50f06ce31f3c92e73c93fc292cf35250f14b58
RolandoHector <69292257+RolandoHector@users.noreply.github.com>
2021-10-19 22:40:41 -0600

    Add files via upload

---
+++ proyecto-php-poo/views/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+
+			<!-- PIE DE PÁGINA -->
+			<footer id="footer">
+				<p>Desarrollado por Los Inges Locos  &copy; <?= date('Y') ?></p>
+			</footer>
+		</div>
+	</body>
+</html>
